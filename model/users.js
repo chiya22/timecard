2e64789824f64ca0568733ca17fed88ad90d6c66
chiya22 <tomoyay@gmail.com>
2021-10-25 06:35:16 +0900

    初期表示をする際のSQLを追加

--- model/users.js
+++ model/users.js
@@ -53,6 +53,20 @@ const remove = async (id) => {
     }
 };
 
+/*
+区分（1：社員、2：バイト）をもとに、
+指定された日付の出勤情報を付帯した、社員情報一覧を返却する
+*/
+const findByKubunWithYyyymmddInfo = async (kubun, yyyymmdd) => {
+    try {
+        const query = "SELECT u.*, yy.yyyymmdd, yy.time_start, yy.time_end, yy.time_start_upd, yy.time_end_upd FROM (SELECT * from users WHERE kubun = '" + kubun + "') AS u LEFT OUTER JOIN ( SELECT * FROM yyyymmdds y WHERE y.yyyymmdd = '" + yyyymmdd + "') yy ON u.id = yy.id_users ORDER BY u.id asc"
+        const retObj =await knex.raw(query)
+        return retObj[0]
+    } catch(err) {
+        throw err
+    }
+};
+
 module.exports = {
     find: find,
     findPKey: findPKey,
