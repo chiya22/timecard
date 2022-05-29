318c0939965fcd654729de6d6f97d4540916e024
chiya22 <tomoyay@gmail.com>
2022-05-29 17:58:34 +0900

    Oatuhを使用してGmailへログインするよう修正

--- util/sendmail.js
+++ util/sendmail.js
@@ -6,15 +6,30 @@ if (process.env.NODE_ENV !== 'production') {
 const send = (title, content) => {
 
     // SMTP情報
+    // const smtp_config = {
+    //     host: process.env.MAIL_HOST,
+    //     port: process.env.MAIL_PORT,
+    //     secure: true,
+    //     auth: {
+    //         user: process.env.MAIL_USER,
+    //         pass: process.env.MAIL_PASS,
+    //     },
+    // }
+
+    // 認証情報
+    const auth = {
+        type         : 'OAuth2',
+        user         : process.env.MAIL_USER,
+        clientId     : process.env.CLIENT_ID,
+        clientSecret : process.env.CLIENT_SECRET,
+        refreshToken : process.env.REFRESH_TOKEN
+    };
+
+    // トランスポート
     const smtp_config = {
-        host: process.env.MAIL_HOST,
-        port: process.env.MAIL_PORT,
-        secure: true,
-        auth: {
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PASS,
-        },
-    }
+        service : 'gmail',
+        auth    : auth
+    };    
 
     let transporter = nodemailer.createTransport(smtp_config);
 
