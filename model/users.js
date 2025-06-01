dc16a537e2ad378efd28410c7b52c4ac171bdf5d
chiya22 <tomoyay@gmail.com>
2025-06-01 09:48:34 +0900

    退職した人を抽出するメソッドを追加

--- model/users.js
+++ model/users.js
@@ -43,6 +43,16 @@ const findByKubunWithYyyymmddInfo = async (kubun, ymd_end, yyyymmdd) => {
 };
 
 /*
+区分（1：社員、2：バイト）と日付をもとに、
+その日付時点で無効な社員情報に指定された日付の出勤情報を付帯した、社員情報一覧を返却する
+*/
+const findEndByKubunWithYyyymmddInfo = async (kubun, ymd_end, yyyymmdd) => {
+  const query = `SELECT u.*, yy.yyyymmdd, yy.time_start, yy.time_end, yy.time_start_upd, yy.time_end_upd, yy.isYuukyuu FROM (SELECT * from users WHERE kubun = '${kubun}' and ymd_end <= '${ymd_end}') AS u LEFT OUTER JOIN ( SELECT * FROM yyyymmdds y WHERE y.yyyymmdd = '${yyyymmdd}') yy ON u.id = yy.id_users ORDER BY u.id asc`;
+  const retObj = await knex.raw(query);
+  return retObj[0];
+};
+
+/*
 IDと日付をもとに、出勤情報を取得する
 */
 const findByUserIDWithYyyymmddInfo = async (id_users, yyyymmdd) => {
@@ -67,6 +77,7 @@ module.exports = {
   find: find,
   findPKey: findPKey,
   findByKubunWithYyyymmddInfo: findByKubunWithYyyymmddInfo,
+  findEndByKubunWithYyyymmddInfo:findEndByKubunWithYyyymmddInfo,
   findByUserIDWithYyyymmddInfo: findByUserIDWithYyyymmddInfo,
   findByYyyymmdd: findByYyyymmdd,
   insert: insert,
